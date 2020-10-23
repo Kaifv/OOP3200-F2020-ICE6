93d59a85fd1a572c0064d6ea146a7f5a337dae56
kaif vakil <kaifvakil02@gmail.com>
2020-10-23 00:54:43 -0400

    Solution for output and input buffer

--- OOP3200-F2020-Lesson3c/GameObject.h
+++ OOP3200-F2020-Lesson3c/GameObject.h
@@ -3,7 +3,7 @@
 #define __GAME_OBJECT__
 
 #include "Vector2D.h"
-
+#include <string>
 class GameObject
 {
 public:
@@ -11,7 +11,8 @@ public:
 	GameObject();
 	GameObject(int id, float x, float y);
 	GameObject(int id, const Vector2D<float>& position);
-
+	GameObject(const std::string& name, int id, float x, float y);
+	GameObject(const std::string& name, int id, const Vector2D<float>& position);
 	// Rule of Three
 	~GameObject(); // Destructor
 	GameObject(const GameObject& other_object); // Copy Constructor
@@ -20,18 +21,23 @@ public:
 	// Accessors
 	Vector2D<float> GetPosition() const;
 	int GetID() const;
+	std::string GetName() const;
 	
 	// Mutators
 	void SetPosition(float x, float y);
 	void SetPosition(const Vector2D<float>& new_position);
 	void SetID(int id);
+	void SetName(const std::string& name);
 
 	// Utility Functions
 	std::string ToString() const;
+	std::string ToFile() const;
 	
 private:
 	int m_id{};
+	std::string m_name;
 	Vector2D<float> m_position;
+	
 };
 
 #endif /* defined (__GAME_OBJECT__) */
